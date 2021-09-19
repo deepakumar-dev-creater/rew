807a363d43aadab0ba11a6dc9f7f5ab7f3e74331
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-09-19 21:50:17 +0530

    added skills section
    Signed-off-by: deepakumar-dev-creater <sean4deepak@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import './index.css';
+import "./index.css";
 import { useEffect } from "react";
 import Profile from "./images/Deepak.JPG";
 import anime from "animejs/lib/anime.es.js";
@@ -109,9 +109,69 @@ function App() {
           <div className="container rounded-full h-10 w-10 bg-blue-700  absolute bubble3 bottom-0"></div>
           <div className="container rounded-full h-10 w-10 bg-blue-700  absolute bubble4 bottom-14"></div>
         </Section>
-        <Section id="about">ABOUT ME</Section>
-        <Section id="contact">contact </Section>
+        <hr />
+        <Section id="about">
+          <p className="text-center md:text-8xl tracking-wide text-7xl">
+            About Me,
+          </p>
+          <div className="container card flex text-center mx-auto">
+            Iam deepak kumar able to create intersting and creative web projects
+            with good ui and ux .I started learning web devleopment on my own at
+            the age of 17 so i can faced many difficulty and obstacles.I am now
+            speciallized in fix frontend issues and also cable of cloning a ui
+            from scrach As the world come in handy on a smartphone so learned
+            flutter development for cross site devlopment.backend devlopment I
+            know web framework like laravel,django using one of these created
+            rest api.My hobbies are learning or getting information of some new
+            technologhy by a computer surfing then finnaly is to keep our body
+            fit..
+          </div>
+        </Section>
+        <hr />
+        <Section id="education">
+          <p className="text-center md:text-8xl tracking-wide text-7xl">
+            Education
+          </p>
+          <div className="container grid grid-cols-2 min-w-full space-y-5">
+            <div className="col-span-2">
+              <div className="container flex flex-col  items-center m-auto">
+                <div className="card m-auto p-5 w-48 md:w-96">
+                  <div className="text-center">
+                    Jeppiaar Engineering College
+                  </div>
+                  <div className="text-center">
+                    B.Tech Information Technology
+                  </div>
+                  <div className="text-center">2018-2022</div>
+                </div>
+              </div>
+            </div>
+            <div className="card w-48 md:w-96 m-auto p-5">
+              <div>Sir Siva Swami Kalalaya Senior Secondary School</div>
+              <div className="text-center">HSC</div>
+              <div className="text-center">2017-2018</div>
+              <div className="text-center">84%</div>
+            </div>
 
+            <div className="card md:w-96 m-auto w-48 p-5">
+              <div className="text-center">Montfort Academy</div>
+              <div className="text-center">SSLC</div>
+              <div className="text-center">2015-2016</div>
+              <div className="text-center">89%</div>
+            </div>
+          </div>
+        </Section>
+        <hr />
+        <Section id="skills">
+          <p className="text-center md:text-8xl tracking-wide text-7xl">
+            Skill
+          </p>
+          <div className="container grid md:grid-cols-2 grid-cols-1 min-w-full md:space-y-10 space-y-5">
+            <div className="grid grid-cols-2"><div>python</div></div>
+            <div></div>
+          </div>
+        </Section>
+        <Section id="contact">contact </Section>
       </div>
     </ScrollingProvider>
   );
